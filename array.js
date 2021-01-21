10890b2f7226a8407ec522f764caeed4fbbbe3b6
Erwin Ramadhan <erwinramadhan35@gmail.com>
2021-01-21 17:45:26 +0700

    [Erwin] Soal nomer 4, membuat variable array dengan minimal 5 itemtampilkan menggunakan forEach

---
+++ array.js
@@ -0,0 +1,12 @@
+const listGames = [
+    "Valorant",
+    "League of Legends",
+    "Mobile Legends",
+    "Arena of Valor",
+    "League of Legends Wildrift",
+    "Genshin Impact"
+];
+    
+listGames.forEach(function(game){
+console.log(game)
+})
