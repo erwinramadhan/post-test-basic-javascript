e8059a8d0ffbe59890b1f7891c7915958c921f25
Erwin Ramadhan <erwinramadhan35@gmail.com>
2021-01-21 17:43:44 +0700

    [Erwin] Tugas nomer 3, membuat variable object dengan minimal 5 key

---
+++ object.js
@@ -0,0 +1,15 @@
+let spesifikasiKomputer = {
+    processor: "AMD Atlhon II X3 425",
+    ram: "8GB DDR3",
+    vga: "NVIDIA GTX 560SE",
+    keyboard: "Gladiator GK-008",
+    mouse: "Fantech",
+    monitor: {
+        nama: "LG 22MK400",
+        resolusi: "1920 x 1080",
+        aspectratio: "16:9",
+        connectivity: "D-Sub, HDMI, Headphone out"
+    }
+};
+
+console.log(spesifikasiKomputer)
